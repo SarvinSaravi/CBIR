d7d41db861f7ce46fe8e1520c98dac40e3917a0b
Moniba <moniba.ravan@gmail.com>
2023-05-27 03:19:30 +0330

    Changing main to compatible with resnet101 & removing preprocess_input

--- main.py
+++ main.py
@@ -14,14 +14,13 @@ from similarity import load_similarity
 def feature_extracting(dataset_path,
                        image_size=(224, 224),
                        ) -> (list, list):
-    images_dict = loading_image_dataset(dataset_path, image_size)
+    images_dict = loading_image_dataset(dataset_path,
+                                        image_size
+                                        )
     image_names = list(images_dict.keys())
-    image_list = list()
-    for img in images_dict.values():
-        img = preprocess_input(img)
-        image_list.append(img)
+    image_list = list(images_dict.values())
 
-    model = load_model(model_name='resnet50',
+    model = load_model(model_name='resnet101',
                        image_size=image_size,
                        n_classes=2,
                        )
@@ -34,8 +33,8 @@ def feature_extracting(dataset_path,
 
 
 def main():
-    query = 47
-    threshold = 0.7
+    query = 13
+    threshold = 0.65
     dataset_path = "dataloading/Selected dataset"
 
     img_names, img_vectors = feature_extracting(dataset_path)
@@ -54,10 +53,9 @@ def main():
     plt.show()
 
     for i in range(len(img_names)):
-        result = measurement(crelu_vectors[query].reshape(1, -1),
-                             crelu_vectors[i].reshape(1, -1),
+        result = measurement(img_vectors[query].reshape(1, -1),
+                             img_vectors[i].reshape(1, -1),
                              )
-        # print(result)
 
         if result > threshold:
             print(result)
