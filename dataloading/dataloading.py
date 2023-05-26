730f8e7f71ac868b9b29a5f1c6a116bfb9643927
Moniba <moniba.ravan@gmail.com>
2023-05-26 18:30:05 +0330

    debugging dataloading

--- dataloading/dataloading.py
+++ dataloading/dataloading.py
@@ -1,7 +1,7 @@
 import keras.utils as ut
+import numpy as np
 
 import os
-
 
 """
 Notes
@@ -9,19 +9,23 @@ __________
     path means direction to the file/folder + the file/folder name
     dir means only direction to the file/folder
 """
+
+
 def loading_an_image(img_path, image_size):
-    """This function loads an image.
+    """This function loads an image and convert to array.
     """
     img = ut.load_img(img_path,
                       target_size=image_size,
                       )
+    img = ut.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
     return img
 
 
 def loading_image_dataset(dataset_path,
                           image_size=(224, 224),
                           ) -> dict:
-    """This function laods a list of images in a given folder path.
+    """This function loads a list of images in a given folder path.
     """
     image_list = dict()  # key: image name, val: image
     all_img_names = os.listdir(dataset_path)
@@ -29,5 +33,5 @@ def loading_image_dataset(dataset_path,
         image_path = dataset_path + '/' + img_name
         img = loading_an_image(image_path, image_size)
         image_list[img_name] = img
-        print(" > Loading Images is Done!")
+    print(" > Loading Images is Done!")
     return image_list
