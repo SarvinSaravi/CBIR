0e77c94de38ced175627bea2136323f2afcaabf1
Moniba <moniba.ravan@gmail.com>
2023-05-30 01:58:03 +0330

    Moving feature extraction to model package & some commenting to clarify.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from dataloading.dataloading import loading_image_dataset
+
 from models import load_model
 from crelu import load_crelu
 from permutation import load_permutations
@@ -9,38 +9,24 @@ from reports.search_texts_via_query import search_texts_via_query
 from text_representation import load_text_representation
 
 
-def feature_extracting(dataset_path,
-                       image_size=(224, 224),
-                       ) -> (list, list):
-    images_dict = loading_image_dataset(dataset_path,
-                                        image_size
-                                        )
-    image_names = list(images_dict.keys())
-    image_list = list(images_dict.values())
+def main():
+    # Initialize
+    query = 10
+    threshold = 0.7
+    k = 400  # text representation
+    dataset_path = "dataloading/Selected dataset"
+    image_size = (224, 224)
 
+    # Feature Extracting
     model = load_model(model_name='resnet101',
                        image_size=image_size,
-                       n_classes=2,
                        )
-    print(" > Loading model is Done!")
-
-    img_vectors = model.predict(np.vstack(image_list))
-    print(img_vectors.shape)
-
-    return image_names, img_vectors
-
-
-def main():
-    query = 10
-    threshold = 0.6
-    k = 400
-    dataset_path = "dataloading/Selected dataset"
-
-    img_names, img_vectors = feature_extracting(dataset_path)
+    img_names, img_vectors = model.extract_feature_vectors(dataset_path=dataset_path,
+                                                           )
     print(" > Making Feature Vectors is Done!")
 
-    images_path = [dataset_path + '/' + img_names[i] for i in range(len(img_names))]
     # Validation Vectors
+    images_path = [dataset_path + '/' + img_names[i] for i in range(len(img_names))]
     search_vectors_via_query(query_path=dataset_path + '/' + img_names[query],
                              images_path=images_path,
                              query_vector=img_vectors[query],
@@ -49,19 +35,22 @@ def main():
                              similarity_func='cosine'
                              )
 
+    # Making CReLU Vectors
     crelu_vectors = load_crelu(img_vectors)
     print(crelu_vectors.shape)
     print(" > Making CreLU Vectors is Done!")
 
+    # Making Permutation Vectors
     permutation_vectors = np.apply_along_axis(load_permutations, axis=1, arr=crelu_vectors)
     print(permutation_vectors.shape)
     print(" > Making Deep Permutation Vectors is Done!")
 
+    # Making Text Strings
     text_strings = list(load_text_representation(item, k) for item in permutation_vectors)
     print(len(text_strings))
     print(" > Making Surrogate Text Representation is Done!")
 
-    # Validation Text strings
+    # Validation Text Strings
     search_texts_via_query(query_path=images_path[query],
                            images_path=images_path,
                            query_string=text_strings[query],
