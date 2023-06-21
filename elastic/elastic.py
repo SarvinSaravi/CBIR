338feb6c8c9e835f34b0065f97eaa0c16c8251cc
SarvinSaravi <sarvin2000@gmail.com>
2023-06-21 12:31:23 +0330

    initial implement indexing approach -> create index and load data

--- elastic/elastic.py
+++ elastic/elastic.py
@@ -1,35 +1,53 @@
 import json
 from elasticsearch import Elasticsearch
+import pandas as pd
+import csv
+
+# preparation mappings
+str_lst = ['T4T4T4T4T4 T2T2T2T2 T7T7T7 T3T3 T1     ',
+           'T8T8T8T8T8 T7T7T7T7 T5T5T5 T1T1 T2     ',
+           'T10T10T10T10T10 T8T8T8T8 T7T7T7 T5T5 T6     '
+           ]
+
+str_lst = [st.rstrip() for st in str_lst]
+K = 5
+data = {}
+index_counting_list = list(reversed(range(1, K + 1)))
+
+for i in index_counting_list:
+    key = 'pos' + str(i)
+    data[key] = {'type': 'keyword'}
+
+mappings = {
+    'properties': data
+}
+
+json_mappings = json.dumps(mappings, indent=4)
+print(json_mappings)
+
 
 # Connect to 'http://localhost:9200'
 es = Elasticsearch("http://localhost:9200")
 
 # Define index name and settings/mappings
-index_name = 'my_test'
+index_name = 'test3'
 settings = {
     'number_of_shards': 2,
     'number_of_replicas': 0
-}
-mappings = {
-    'properties': {
-        'title': {'type': 'text'},
-        'identifier': {'type': 'keyword'},
-        'description': {'type': 'text'}
-    }
 }
 
 # Create index with defined settings/mappings
 es.indices.create(index=index_name, mappings=mappings, settings=settings)
 
 # inject data to index
-body = {
-    'title': 'Money Heist',
-    'identifier': 'idx1',
-    'description': 'This series released in 2017'
-}
-es.index(index=index_name, id=1, document=body)
+tmp_id = 1
+for st in str_lst:
+    mid_list = st.split(' ')
+    # mid_list.reverse()
+    for i, x in zip(index_counting_list, mid_list):
+        key = 'pos' + str(i)
+        data[key] = x
 
-# Get data from index
-data = es.get(index=index_name, id=1)['_source']
-data = json.dumps(data, indent=2)
-print(data)
+    es.index(index=index_name, id=tmp_id, document=data)
+    tmp_id += 1
+
