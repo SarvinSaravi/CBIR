8528c91b7ea59a09b214169af735dcbf4631e079
SarvinSaravi <sarvin2000@gmail.com>
2023-06-22 11:20:08 +0330

    function of searching

--- elastic/elastic_search.py
+++ elastic/elastic_search.py
@@ -1,28 +1,34 @@
-import json
 from elasticsearch import Elasticsearch
 
-# Connect to 'http://localhost:9200'
-es = Elasticsearch("http://localhost:9200")
 
-index_name = 'test3'
+def elastic_searching(focus_index, query_text):
+    # Connect to 'http://localhost:9200'
+    es = Elasticsearch("http://localhost:9200")
 
-# refresh whole index
-es.indices.refresh(index=index_name)
+    index_name = focus_index
 
-query_string = 'T42T42T42T42T42 T32T32T32T32 T12T12T12 T95T95 T2'
-query_list = query_string.split(' ')
+    # refresh whole index
+    es.indices.refresh(index=index_name)
 
-data_list = [{"match": {"pos" + str(i): pos}} for i, pos in enumerate(reversed(query_list), start=1)]
+    query_string = query_text
+    query_list = query_string.split(' ')
 
+    data_list = [{"match": {"pos" + str(i): pos}} for i, pos in enumerate(reversed(query_list), start=1)]
 
-my_query2 = {
-    "bool": {
-        "should": data_list, "minimum_should_match": 1
+    my_query = {
+        "bool": {
+            "should": data_list, "minimum_should_match": 1
+        }
     }
-}
 
-resp = es.search(index=index_name, query=my_query2)
-print("Got %d Hits:" % resp['hits']['total']['value'])
-for hit in resp['hits']['hits']:
-    print("score of this result is %s" % hit["_score"])
-    print(hit["_source"])
+    resp = es.search(index=index_name, query=my_query)
+    print("Got %d Hits:" % resp['hits']['total']['value'])
+    for hit in resp['hits']['hits']:
+        print("score of this result is %s" % hit["_score"])
+        print(hit["_source"])
+    print()
+
+
+# s = 'T4T4T4T4T4 T2T2T2T2 T5T5T5 T1T1 T1017'
+# f_index = 'test'
+# elastic_searching(f_index, s)
