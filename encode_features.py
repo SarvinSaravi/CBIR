46a2f584a18a1a4582db6fdf4f57bdeb82680b75
Moniba <moniba.ravan@gmail.com>
2023-07-02 02:57:56 +0330

    Adding encode_features and save encoded_features as .csv file.

--- encode_features.py
+++ encode_features.py
@@ -1,10 +1,51 @@
-from dataloading.dataloading import loading_from_csv
+from dataloading.dataloading import loading_from_npz
+import numpy as np
+from reports.save_in_files import save_in_csv
+import time
+from crelu import load_crelu
+from permutation_text import vector2text_processing
+from partitioning import partitioning_process
 
 def encode_features():
-    # load features
-    data = loading_from_csv(file_name="Selected dataset_features.csv")
-    img_names, img_vectors = [row[0] for row in data], [row[1] for row in data]
+    K = 42
 
+    start_time = time.time()
+    # Loading features
+    data = dict(loading_from_npz(file_name="Selected dataset_features.npz"))
+    img_names, img_vectors = np.array(list(data.keys())), np.array(list(data.values()))
+
+    # Making CReLU Vectors
+    crelu_vectors = load_crelu(img_vectors)
+    print(crelu_vectors.shape)
+    print(" > Making CreLU Vectors is Done!")
+
+    """ *> string_list should be indexed in ElasticSearch """
+    string_list = vector2text_processing(crelu_vectors, K)
+    print("| string list length | = " + str(len(string_list)))
+
+
+    """
+        Partitioning Process
+        *> part_k is a parameter same as k but for every part of partitions
+        *> num_sections is the number of partitions that vectors divided into
+        *> L is the Length of every partition that vectors divided into
+        *> partition_string_list should be indexed in ElasticSearch
+    """
+    # print(" > Process of partitioning!")
+    # partition_string_list = partitioning_process(crelu_vectors, part_k=5, length=L)
+    # partition_string_list = partitioning_process(crelu_vectors, part_k=20, num_sec=num_sections)
+
+    # save output
+    file_name = "selected data_encoded_features"
+    save_in_csv(data=(img_names, string_list),  # each colmun of .csv
+                file_name=file_name,
+                )
+    # time measurement
+    end_time = time.time()
+    duration = end_time - start_time
+
+    print("Encoding features took %s seconds" % duration)
+    return
 
 
 if __name__ == "__main__":
