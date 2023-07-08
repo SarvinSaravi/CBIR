df0ab97d275cb1952514361aec7f8624aedec4b5
SarvinSaravi <sarvin2000@gmail.com>
2023-07-08 21:28:15 +0330

    implement search and evaluation (Main Commit)

--- evaluation/evaluation_functions.py
+++ evaluation/evaluation_functions.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import subprocess
+import os
 
 
 def get_queries():
-    df = pd.read_csv('baseline_result_200806.dat', delimiter='\t', header=None)
+    """
+        this function read baseline_result_200806.dat and make a list of queries(image titles)
+        instead of baseline_result_200806.dat can use:
+           evaluation/he_wgc_result_200806.dat
+           evaluation/perfect_result.dat
+    """
+    file_path = os.path.join(os.path.dirname(__file__), 'baseline_result_200806.dat')
+    df = pd.read_csv(file_path, delimiter='\t', header=None)
     query_list = []
     for i in range(df.size):
         query_list.append(df.iloc[i][0][0:10])
@@ -11,7 +19,15 @@ def get_queries():
     return query_list
 
 
-def result_assessment(results: dict, file_path):
+def result_assessment(results: dict, file_name):
+    """
+            result is a python dictionary (similar to JSON) that:
+                    keys = query (image title)
+                    values = elasticsearch response for this query + a dictionary form like:
+                        keys = similar images (image title)
+                        values = score of this similar image (How similar is it)
+    """
+    file_path = os.path.join(os.path.dirname(__file__), file_name)
     with open(file_path, 'w') as file:
         for query, response in results.items():
             result_line = query + ' '
@@ -23,20 +39,20 @@ def result_assessment(results: dict, file_path):
                 rank += 1
             file.write(result_line + '\n')
 
-    command = 'python holidays_map.py ' + file_path
-    subprocess.call(command, shell=True)
+    # holiday_script = os.path.join(os.path.dirname(__file__), 'holidays_map.py')
+    # command = 'python ' + holiday_script + ' ' + file_path
+    # subprocess.call(command, shell=True)
 
-
-res = {
-    '125800.jpg': {
-        '125800.jpg': 20.6656,
-        '125801.jpg': 2.4323,
-        '125802.jpg': 2.63278
-    },
-    '144400.jpg': {
-        '144401.jpg': 5.23232,
-        '144400.jpg': 12.96878
-    }
-}
-
-result_assessment(res, 'map.dat')
+# test-case for understand result structure and make result file
+# res = {
+#     '125800.jpg': {
+#         '125800.jpg': 20.6656,
+#         '125801.jpg': 2.4323,
+#         '125802.jpg': 2.63278
+#     },
+#     '144400.jpg': {
+#         '144401.jpg': 5.23232,
+#         '144400.jpg': 12.96878
+#     }
+# }
+# result_assessment(res, 'map.dat')
--- search_results.py
+++ search_results.py
@@ -1,41 +1,50 @@
 import time
 import numpy as np
+import os
 
 from dataloading.dataloading import loading_from_npz
 from elastic import elastic_search_by_vector
+from evaluation.evaluation_functions import get_queries, result_assessment
+
+# os.chdir("D:\\Python\\visual_search\\visual-search")
 
 
 def search_results():
     # Initialize
-    K = 400
-    index_name = 'title_data_k%s' % K
+    K = 42
+    index_name = 'm_title_data_k%s' % K
     # Loading features
-    data = dict(loading_from_npz(file_name="Selected dataset_features.npz"))
+    data = dict(loading_from_npz(file_name="Main dataset_features.npz"))
     img_names, img_vectors = np.array(list(data.keys())), np.array(list(data.values()))
 
     start_time = time.time()
-    # search and compare pictures together
-    result = np.zeros(2500).reshape(50, 50)
 
-    for i in range(len(img_names)):
-        query_vector = img_vectors[i]
-        search_answer = elastic_search_by_vector(index_name, query_vector, K)
-        for ans_id, ans_score in search_answer.items():
-            ans_id = int(ans_id)
-            result[i][ans_id - 1] = ans_score
+    # start search by get queries from evaluation package
+    query_list = get_queries()
+    result = {}
 
-    # save output
-    filename = 'result_K%s.csv' % K
-    np.savetxt("results/csv/" + filename, result, delimiter=',', fmt='%s')
-    print(" > Searching in Elasticsearch is Done And Results saved in ", filename)
+    # main search flow
+    for query in query_list:
+        if query in img_names:
+            # find the location of the query vector in our structure
+            query_name_index = np.where(img_names == query)[0]
+            query_vector = img_vectors[query_name_index]
+            # search in elastic index (K is for vector to text transformation)
+            search_answer = elastic_search_by_vector(index_name, query_vector, K)
+            result[query] = search_answer
+
+    print(" > Searching in Elasticsearch is Done!")
+
+    # save output and evaluate mAP
+    filename = 'result_K%s.dat' % K
+    result_assessment(result, filename)
+    print(" > Results Assessment done and file saved in ", filename)
 
     # time measurement
     end_time = time.time()
     duration = end_time - start_time
 
     print("The code took %s seconds to execute with K = %s" % (duration, K))
-
-    print()
 
 
 if __name__ == '__main__':
