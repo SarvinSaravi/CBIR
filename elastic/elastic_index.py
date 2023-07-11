1b3fb0964a8f2f877c06b60f75ebfd9e8f6796e0
SarvinSaravi <sarvin2000@gmail.com>
2023-07-11 14:53:43 +0330

    implement indexing for all image with k=42

--- elastic/elastic_index.py
+++ elastic/elastic_index.py
@@ -1,48 +1,18 @@
 from elasticsearch import Elasticsearch
 
 
-def elastic_indexing(data_list, k, focus_index, shard_number=2, replica_number=0):
-    # preparation mappings
-    str_lst = [st.rstrip() for st in data_list]
-    K = k
-    data = {}
-    index_counting_list = list(reversed(range(1, K + 1)))
-
-    for i in index_counting_list:
-        key = 'pos' + str(i)
-        data[key] = {'type': 'keyword'}
-
-    mappings = {
-        'properties': data
-    }
-
-    # json_mappings = json.dumps(mappings, indent=4)
-    # print(json_mappings)
-
-    # Connect to 'http://localhost:9200'
-    es = Elasticsearch("http://localhost:9200")
-
-    # Define index name and settings/mappings
-    index_name = focus_index
-    settings = {
-        'number_of_shards': shard_number,
-        'number_of_replicas': replica_number
-    }
-
-    # Create index with defined settings/mappings
-    es.indices.create(index=index_name, mappings=mappings, settings=settings)
-
-    # inject data to index
-    tmp_id = 1
-    for st in str_lst:
-        mid_list = st.split(' ')
-        # mid_list.reverse()
-        for i, x in zip(index_counting_list, mid_list):
-            key = 'pos' + str(i)
-            data[key] = x
-
-        es.index(index=index_name, id=str(tmp_id), document=data)
-        tmp_id += 1
+def elastic_indexing(title_list, data_list, focus_index, indexing_method, shard_number=2, replica_number=0 ):
+    if indexing_method == 'same_exact_phrase_with_separator':
+        return elastic_indexing_idea3(title_list, data_list, focus_index, shard_number, replica_number)
+    elif indexing_method == 'fuzzy_search':
+        print(' to do ')  # todo
+    elif indexing_method == 'remove_frequency':
+        print(' to do ')  # todo
+    elif indexing_method == 'prefix_search':
+        print(' to do ')  # todo
+    else:
+        print(" >>> Please clarify the indexing method <<< ")
+    return
 
 
 def elastic_indexing_with_titles(title_list, data_list, k, focus_index, shard_number=2, replica_number=0):
@@ -89,7 +59,7 @@ def elastic_indexing_with_titles(title_list, data_list, k, focus_index, shard_nu
         tmp_id += 1
 
 
-def elastic_indexing_idea3(title_list, data_list, focus_index, shard_number=2, replica_number=0):
+def elastic_indexing_idea3(title_list, data_list, focus_index, shard_number, replica_number):
     # preparation mappings
     str_lst = [st.rstrip() for st in data_list]
 
