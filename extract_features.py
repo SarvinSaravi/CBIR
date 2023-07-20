c23960063c088d84669e409535ac4d37d68e12b6
Moniba <moniba.ravan@gmail.com>
2023-07-20 19:34:40 +0330

    Adding r-mac version 2 + pcawhitening.

--- extract_features.py
+++ extract_features.py
@@ -2,23 +2,31 @@ import time
 
 from models import load_model
 from reports.save_in_files import save_in_npz
-
+from models.rmac_v2 import RMAC
 
 def extract_features():
     # Start timing
     start_time = time.time()
-    dataset_name = "Selected dataset"
-    dataset_path = "dataloading/Selected dataset"
+    dataset_name = "Main dataset"
+    dataset_path = "dataloading/Main dataset"
     image_size = (224, 224)
     model_name = "resnet101"
 
-    model = load_model(model_name=model_name,
-                       image_size=image_size,
-                       rmac=True,
-                       )
-    img_names, img_vectors = model.extract_feature_vectors(dataset_path=dataset_path,
-                                                           )
-    images_path = [dataset_path + '/' + img_names[i] for i in range(len(img_names))]
+    # model = load_model(model_name=model_name,
+    #                    image_size=image_size,
+    #                    rmac=True,
+    #                    )
+    # img_names, img_vectors = model.extract_feature_vectors(dataset_path=dataset_path,
+                                                        #   )
+                                                        
+    rmac = RMAC(scales=[550, 800, 1050],
+                levels=3,
+                pca = True,
+                verbose= True
+                )
+    
+    img_names, img_vectors = rmac.extract_features(dataset_path=dataset_path)
+    
 
     # save output
     file_name = dataset_name + "_features"
