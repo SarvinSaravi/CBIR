dc92d1a890633b6fd7f06318a95af03d23fb38ea
Moniba <moniba.ravan@gmail.com>
2023-07-28 17:41:16 +0330

    Creating resnet101+rmac model or only resne101.

--- dataloading/dataloading.py
+++ dataloading/dataloading.py
@@ -12,14 +12,21 @@ __________
 """
 
 
-def loading_an_image(img_path):
+def loading_an_image(img_path,
+                     image_size,
+                     ):
     """This function loads an image.
     """
-    img = cv2.imread(img_path)
+    img = ut.load_img(img_path,
+                      target_size=image_size,
+                      )
+    img = ut.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
     return img
 
 
 def loading_image_dataset(dataset_path,
+                          image_size,
                           ) -> dict:
     """This function loads a list of images in a given folder path.
     """
@@ -27,7 +34,8 @@ def loading_image_dataset(dataset_path,
     all_img_names = os.listdir(dataset_path)
     for img_name in all_img_names:
         image_path = dataset_path + '/' + img_name
-        img = loading_an_image(image_path)
+        img = loading_an_image(image_path,
+                               image_size)
         image_list[img_name] = img
     print(" > Loading Images is Done!")
     return image_list
